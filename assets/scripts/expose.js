60e4b688ffce86defff1c39a1c928b01100e9281
ndpya <nduangpanya@ucsd.edu>
2026-05-03 21:33:30 -0700

    Installed packages and started on expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const hornSelector = document.getElementById("horn-select");
+  if(!hornSelector){
+    console.log("horn selector not found");
+  }
+  hornSelector.addEventListener('change', (event) => {
+    const selection = event.target.value;
+    if(selection == "air-horn"){
+      console.log("air horn");
+      
+    }
+    else if(selection == "car-horn"){
+      console.log("car horn");
+    }
+    else{
+        console.log("party horn");
+    }
+  });
 }
+
+
